4e846bd4e11095467a40952acce4676493acc129
Aleksandr Osipov <osipov.as89@gmail.com>
2014-08-27 17:53:49 +0400

    Refactor code

--- __init__.py
+++ __init__.py
@@ -1,9 +1,23 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Query
+import re
+from sqlalchemy import func
+from sqlalchemy.sql.expression import ClauseElement, _literal_as_text
+from sqlalchemy.sql.visitors import replacement_traverse
+from sqlalchemy.orm import Query, class_mapper
+from sqlalchemy.orm.base import manager_of_class, _generative
 from sqlalchemy.dialects import registry
-from .util import ModelLoader, Proxy
 
-registry.register('sphinxql', 'common.fulltext.sphinxql', 'Dialect')
+registry.register('sphinxql', 'sphinxqla.dialect', 'Dialect')
+
+
+def escape_match(value):
+    return re.sub(r'([=\(\)|\-!@~"&/\\\^\$=])', r'\\\1', value)
+
+
+class MatchClause(ClauseElement):
+    __visit_name__ = 'match'
+
+    def __init__(self, value):
+        self.value = value
 
 
 class SphinxQuery(Query):
@@ -36,13 +50,55 @@ class SphinxQuery(Query):
             return int(meta[1])
         return 0
 
-    def proxy(self, session, model_cls):
-        loader = ModelLoader(session, model_cls)
-        return Proxy(self, loader)
+    @_generative(Query._no_statement_condition, Query._no_limit_offset)
+    def match(self, **kwargs):
+        expression = ' '.join([
+            u'@{} {}'.format(field, escape_match(text))
+            for field, text in kwargs.iteritems()
+        ])
+        self.add_match(expression)
+
+    @_generative(Query._no_statement_condition, Query._no_limit_offset)
+    def match_expression(self, expression, *args, **kwargs):
+        escaped_args = tuple(v for v in args)
+        escaped_kwargs = {k: escape_match(v) for k, v in kwargs.iteritems()}
+        expression = expression.format(*escaped_args, **escaped_kwargs)
+        self.add_match(expression)
+
+    def add_match(self, value):
+        def replace(node):
+            if isinstance(node, MatchClause):
+                return MatchClause(' '.join([node.value, value]))
+            return node
+
+        if self._criterion is not None:
+            self._criterion = replacement_traverse(self._criterion, {}, replace)
+        else:
+            criterion = _literal_as_text(MatchClause(value))
+            self._criterion = self._adapt_clause(criterion, True, True)
 
 
-def sphinx_sessionmaker(urn, **kwargs):
-    engine = create_engine(urn, pool_recycle=60)
-    if not kwargs.get('query_cls', None):
-        kwargs.update({'query_cls': SphinxQuery})
-    return sessionmaker(bind=engine, **kwargs)
+class BulkIdProxy(object):
+
+    def __init__(self, session, identities, cls, key=None):
+        self._session = session
+        self._cls = cls
+        self._identities = identities
+
+        if isinstance(key, basestring):
+            manager = manager_of_class(cls)
+            self._key = manager[key]
+        elif key is None:
+            mapper = class_mapper(cls)
+            primary_keys = mapper.primary_key
+            assert len(primary_keys) == 1
+            self._key = primary_keys[0]
+        else:
+            self._key = key
+
+    def count(self):
+        return len(self._identities)
+
+    def __getitem__(self, item):
+        keys = self._identities[item]
+        return self._session.query(self._cls).filter(self._key.in_(keys)).all()
